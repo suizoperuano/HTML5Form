5215c85a46928f22c0f72c6e0ac8e717a2dea2b2
suizoperuano <andrechedel@hotmail.com>
2023-01-22 23:21:01 +0000

    lastone

--- states.js
+++ states.js
@@ -1,19 +1,30 @@
-document.body.style.backgroundColor = "red";
 
-var mypar = document.getElementById("inputState");
-var newParagraph = document.createElement("p");
-newParagraph.text = "this is great";
-mypar.appendChild(myParagraph);
+let states = ["Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware", "Florida", "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey", "New Mexico", "New York", "North Carolina", "North Dakota", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Vermont", "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming"];
 
+let select = document.getElementById("inputState");
 
+for (let i = 0; i < states.length; i++) {
+    let option = document.createElement("option");
+    option.value = states[i];
+     option.text = states[i];
+    select.appendChild(option);
+}
 
-var states = ["Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware", "Florida", "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey", "New Mexico", "New York", "North Carolina", "North Dakota", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Vermont", "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming"];
+function validate() {
+  let input_1 = document.getElementById("card-number").value;
+  let input_2 = document.getElementById("cvc-number").value;
+  let input_3 = document.getElementById("first-name").value;
+  let input_4 = document.getElementById("last-name").value;
+  let input_5 = document.getElementById("city").value;
+  let input_6 = document.getElementById("inputState").value;
+  let input_7 = document.getElementById("postal-code").value;
 
-    var select = document.getElementById("inputState");
+  let alert_textbox = document.getElementById("alert-field");
 
-    for (var i = 0; i < states.length; i++) {
-        var option = document.createElement("option");
-        option.value = states[i];
-        option.text = states[i];
-        select.appendChild(option);
-    }
+  if (input_1 == "" || input_2 == "" || input_3 == "" || input_4 == "" || input_5 == "" || input_6 == "" || input_6 == "pick a state"|| input_7 == "") {
+    alert_textbox.style.display = "block";
+  }
+  else {
+    alert_textbox.style.display = "none";
+  }
+  }
